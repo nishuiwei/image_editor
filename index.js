6adbf0e9f9d749da729a36585e79ecb87a9cd971
卫慧杰 <w920098695@sina.cn>
2022-07-30 23:29:10 +0800

    feat: 上传文件点击事件

---
+++ index.js
@@ -0,0 +1,6 @@
+const fileInput = document.querySelector('.file-input'),
+	chooseImgBtn = document.querySelector('.choose-img')
+
+chooseImgBtn.addEventListener('click', () => {
+	fileInput.click()
+})
