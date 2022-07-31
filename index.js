4ac00a96fdea76a79f08b25e2390393f3e27a15f
卫慧杰 <w920098695@sina.cn>
2022-07-31 23:23:54 +0800

    feat: 现实用户选择的图像

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const fileInput = document.querySelector('.file-input'),
+	previewImg = document.querySelector('.preview-img img'),
 	chooseImgBtn = document.querySelector('.choose-img')
 
+const loadImage = () => {
+	let file = fileInput.files[0] // 获取用户选择的图片
+	if (!file) return // 如果没有选择，return 出去
+	// console.log(file)
+	previewImg.src = URL.createObjectURL(file)
+	previewImg.addEventListener('load', () => {
+		document.querySelector('.container').classList.remove('disable')
+	})
+}
+
+fileInput.addEventListener('change', loadImage)
 chooseImgBtn.addEventListener('click', () => {
 	fileInput.click()
 })
